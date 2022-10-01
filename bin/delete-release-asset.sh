6169a2fe7e5f6d96260915f539d6944d611a606d
William Merfalen <wmerfalen@gmail.com>
2022-10-01 02:46:21 -0600

    feat: delete-release-asset <USER> <REPO> <ASSET_ID>

---
+++ bin/delete-release-asset.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $# -lt 3 ]]; then
+	echo 'Usage: delete-release-asset.sh USER REPO ASSET_ID'
+	exit 1
+fi
+
+echo 'Please note, this endpoint doesnt appear to have a response body when an asset is successfully deleted'
+
+GITHUB_USER="$1"
+GITHUB_REPO="$2"
+GITHUB_ASSET_ID="$3"
+
+echo "GITHUB_ASSET_ID: '${GITHUB_ASSET_ID}'"
+echo "GITHUB_USER: '${GITHUB_USER}'"
+echo "GITHUB_REPO: '${GITHUB_REPO}'"
+
+curl \
+		-X DELETE \
+	  -H "Accept: application/vnd.github+json" \
+	  -H "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
+  	https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases/assets/${GITHUB_ASSET_ID}
