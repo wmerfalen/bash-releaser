c7920394b72a0280e586b00313d21477b182d21c
William Merfalen <wmerfalen@gmail.com>
2022-10-01 00:12:07 -0600

    feat: list and create releases

--- bin/create.sh
+++ bin/create.sh
@@ -5,20 +5,19 @@ if [[ $# -lt 4 ]]; then
 	exit 1
 fi
 
-GITHUB_USER=$1
-GITHUB_REPO=$2
-GITHUB_TAG_NAME=$3
-GITHUB_NAME=$4
+GITHUB_USER="$1"
+GITHUB_REPO="$2"
+GITHUB_TAG_NAME="$3"
+GITHUB_NAME="$4"
 
 echo "GITHUB_TAG_NAME: '${GITHUB_TAG_NAME}'"
 echo "GITHUB_USER: '${GITHUB_USER}'"
 echo "GITHUB_REPO: '${GITHUB_REPO}'"
 echo "GITHUB_NAME: '${GITHUB_NAME}'"
 
-exit 0
 curl \
 		-X POST \
 	  -H "Accept: application/vnd.github+json" \
 	  -H "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
 		https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/releases \
-  	-d '{"tag_name":"'${GITHUB_TAG_NAME}'","target_commitish":"master","name":"'${GITHUB_NAME}'","body":"body","draft":false,"prerelease":false,"generate_release_notes":false}'
+  	-d '{"tag_name":"'${GITHUB_TAG_NAME}'","name":"'${GITHUB_NAME}'","body":"body","draft":false,"prerelease":false,"generate_release_notes":false}'
--- bin/list.sh
+++ bin/list.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-GITHUB_USER=$1
-GITHUB_REPO=$2
+if [[ $# -lt 2 ]]; then
+	echo 'Usage: list.sh USER REPOSITORY'
+	echo 'Example: list.sh dashhive bls'
+	exit 1
+fi
+
+GITHUB_USER="$1"
+GITHUB_REPO="$2"
 curl \
 	  -H "Accept: application/vnd.github+json" \
 	  -H "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
